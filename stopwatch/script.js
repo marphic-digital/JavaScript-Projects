de1c0da9b478289a31f483ec8ba82e26a0881dc7
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2024-05-31 21:42:02 +0500

    stopwatch

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -13,25 +13,37 @@ function startTimer(){
         elapsedTime = Date.now() - startTime;
         clockEl.textContent = formateTime(elapsedTime);
     }, 10);
+
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
 }
 
 function formateTime(){
-    const miliSeconds = Math.floor((elapsedTime % 1000) / 10);
+    const milliSeconds = Math.floor((elapsedTime % 1000) / 10);
     const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
-    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60) / (1000 * 60)));
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     return (
+        (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
         (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
         (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") + "." +
-        (miliSeconds > 9 ? miliSeconds: "0" + milliSeconds)
+        (milliSeconds > 9 ? milliSeconds: "0" + milliSeconds)
     )
     
     
 }
 function stopTimer(){
-    console.log('stop')
+    clearInterval(timeInterval);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
 }
 function resetTimer(){
-    console.log('reset')
+    clearInterval(timeInterval);
+
+    elapsedTime = '0';
+    clockEl.textContent = '00:00:00'
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
 }
 
 startBtnEl.addEventListener('click', startTimer);
