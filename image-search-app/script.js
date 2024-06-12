a0b5cb6c52dfd4d3f79faf9da2ff18c47d7e74ca
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2024-06-12 23:15:01 +0500

    Add files via upload

--- image-search-app/script.js
+++ image-search-app/script.js
@@ -1,3 +1,4 @@
+'use strict'
 const accessKey = 'zn_ICEVRU812yKjNNQRkjghT2ftP94_yZAy6ePat3tI';
 
 const formEl = document.querySelector('form');
@@ -6,13 +7,24 @@ const searchResultsEl = document.querySelector('search-results');
 const showButtonEl = document.getElementById('show-btn');
 
 let inputData = '';
+let page = 1;
 
-function searchImg(){
+async function searchImg(){
     inputData = searchInputEl.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    if (page === 1){
+        searchResultsEl.innerHTML = " ";
+    }
+
+    const results = data.results;
+    console.log(results);
 }
 
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
+    page = 1;
     searchImg();
 })
