2dce9b5522a11c949f904735b9b5704bcb73a321
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2024-07-03 22:39:27 +0500

    Add files via upload

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const checkBtnEl = document.querySelector('.check');
+
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('#number').textContent = randomNumber;
+
+checkBtnEl.addEventListener('click', function () {
+  const inputEl = Number(document.querySelector('#input').value);
+
+  if (!inputEl) {
+    alert(`Please Enter a number between 1 to 20.`);
+  } else if (inputEl < randomNumber) {
+    document.querySelector('.low').textContent = '⬇ Too Low';
+  } else if (inputEl > randomNumber) {
+    document.querySelector('.low').textContent = '⬆ Too High';
+  } else if (inputEl === randomNumber) {
+    document.querySelector('.low').textContent = '🎉 Correct Number';
+  }
+});
