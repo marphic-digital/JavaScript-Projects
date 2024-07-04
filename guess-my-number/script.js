e6c499201659243e4dc0d569cd41363ac5441efd
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2024-07-04 20:08:49 +0500

    logic-implemented

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,20 +1,39 @@
 'use strict';
 
 const checkBtnEl = document.querySelector('.check');
+const pEl = document.querySelector('.low');
+const scoreEl = document.querySelector('.label-score');
+let score = 20;
 
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('#number').textContent = randomNumber;
+
+let secretNumber = document.querySelector('#number');
+const guess = (secretNumber.textContent = randomNumber);
 
 checkBtnEl.addEventListener('click', function () {
   const inputEl = Number(document.querySelector('#input').value);
 
   if (!inputEl) {
-    alert(`Please Enter a number between 1 to 20.`);
-  } else if (inputEl < randomNumber) {
-    document.querySelector('.low').textContent = '⬇ Too Low';
-  } else if (inputEl > randomNumber) {
-    document.querySelector('.low').textContent = '⬆ Too High';
-  } else if (inputEl === randomNumber) {
-    document.querySelector('.low').textContent = '🎉 Correct Number';
+    alert(`Enter a number between 1 to 20`);
+  } else if (inputEl < guess) {
+    if (score > 1) {
+      pEl.innerHTML = `⬇ Too Low`;
+      score--;
+      scoreEl.innerHTML = `Score: ${score}`;
+    } else {
+      pEl.innerHTML = `😢 You lost the game!`;
+      scoreEl.innerHTML = `Score: 0`;
+    }
+  } else if (inputEl > guess) {
+    if (score > 1) {
+      pEl.innerHTML = `⬆ Too High`;
+      score--;
+      scoreEl.innerHTML = `Score: ${score}`;
+    } else if ((score = 0)) {
+      pEl.innerHTML = `😢 You lost the game!`;
+      scoreEl.innerHTML = `Score: 0`;
+    }
+  } else if (inputEl === guess) {
+    pEl.innerHTML = `🎉 Correct Number`;
   }
 });
