05f6b50462fecf850d6885c796d369314253af2c
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2024-07-07 20:50:18 +0500

    js-added

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtns = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', openModal);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Backspace' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
