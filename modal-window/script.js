0d75e43dcf24ebb9ffbd987c175627ecba31c78d
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2024-07-08 18:46:47 +0500

    project-completed

--- modal-window/script.js
+++ modal-window/script.js
@@ -2,29 +2,29 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const closeModalBtn = document.querySelector('.close-modal');
-const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtnEl = document.querySelector('.close-modal');
+const showModalBtnEl = document.querySelectorAll('.show-modal');
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-for (let i = 0; i < showModalBtns.length; i++) {
-  showModalBtns[i].addEventListener('click', openModal);
+for (let i = 0; i < showModalBtnEl.length; i++) {
+  showModalBtnEl[i].addEventListener('click', showModal);
 }
 
-closeModalBtn.addEventListener('click', closeModal);
+closeModalBtnEl.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Backspace' && !modal.classList.contains('hidden')) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
