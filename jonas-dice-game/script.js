c47770fdaf8982217f237109d039d04b564610cd
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2024-07-10 20:57:00 +0500

    practice

--- jonas-dice-game/script.js
+++ jonas-dice-game/script.js
@@ -1,6 +1,39 @@
 'use strict';
 
-const score0El = document.querySelector('#score--0');
+const score0El = document.getElementById('score--0');
+const scoreEl = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// BUTTON ELEMENT
+
+const btnNewEl = document.querySelector('.btn--new');
+const btnrollEl = document.querySelector('.btn--roll');
+const btnholdEl = document.querySelector('.btn--hold');
+
+diceEl.classList.add('hidden');
+let activePlayer = 0;
+let currentScore = 0;
+
+btnrollEl.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // console.log(dice);
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
+
+/*  const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
@@ -62,4 +95,4 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
-});
+}); */
